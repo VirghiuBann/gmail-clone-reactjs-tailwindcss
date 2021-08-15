82959b9a46fa7f46153d261e253f8d4ce81b3927
virghiu <virghiu@gmail.com>
2021-08-14 23:06:08 -0400

    add sidebar right

--- src/components/Body.js
+++ src/components/Body.js
@@ -1,7 +1,50 @@
 import React from 'react';
+import { mdiCheckboxBlankOutline } from '@mdi/js';
+import { mdiMinusBox } from '@mdi/js';
+import { mdiMenuDown } from '@mdi/js';
+import { mdiRefresh } from '@mdi/js';
+import { mdiDotsVertical } from '@mdi/js';
+import { mdiKeyboard } from '@mdi/js';
+import Icon from '@mdi/react';
 
 function Body() {
-  return <div className='flex-1 bg-green-400'>this is body</div>;
+  const iconSize = '20px';
+  const iconColor = '#6B7280';
+  return (
+    <div className='flex-1 400'>
+      <div className='h-10 border-b border-gray-300 flex justify-between'>
+        <div className='flex align-center'>
+          <button>
+            <Icon
+              path={mdiCheckboxBlankOutline}
+              size={iconSize}
+              color={iconColor}
+            />
+          </button>
+          <button>
+            <Icon path={mdiMinusBox} size={iconSize} color={iconColor} />
+          </button>
+          <button>
+            <Icon path={mdiMenuDown} size={iconSize} color={iconColor} />
+          </button>
+          <button>
+            <Icon path={mdiRefresh} size={iconSize} color={iconColor} />
+          </button>
+          <button>
+            <Icon path={mdiDotsVertical} size={iconSize} color={iconColor} />
+          </button>
+        </div>
+        <div className='flex align-center'>
+          <button>
+            <Icon path={mdiKeyboard} size={iconSize} color={iconColor} />
+          </button>
+          <button>
+            <Icon path={mdiMenuDown} size={iconSize} color={iconColor} />
+          </button>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default Body;
--- src/components/SidebarRight.js
+++ src/components/SidebarRight.js
@@ -1,7 +1,43 @@
 import React from 'react';
 
 function SidebarRight() {
-  return <div className='w-10 bg-yellow-300'>hi</div>;
+  return (
+    <div className='w-14 h-full border-l border-gray-200 flex flex-col'>
+      <div className='w-full flex justify-center items-center my-3'>
+        <div className='h-6 w-6'>
+          <img
+            src='https://www.gstatic.com/companion/icon_assets/calendar_2020q4_2x.png'
+            alt='calendar'
+          />
+        </div>
+      </div>
+      <div className='w-full flex justify-center items-center my-3'>
+        <div className='bg-transparent rounded-full hover:shadow-md hover:bg-gray-100 sidebar__btn__right'>
+          <img
+            src='https://www.gstatic.com/companion/icon_assets/keep_2020q4v3_2x.png'
+            alt='keep'
+            className='object-contain h-6 w-6'
+          />
+        </div>
+      </div>
+      <div className='w-full flex justify-center items-center my-3'>
+        <div className='h-6 w-6'>
+          <img
+            src='https://www.gstatic.com/companion/icon_assets/tasks2_2x.png'
+            alt='keep'
+          />
+        </div>
+      </div>
+      <div className='w-full flex justify-center items-center my-3'>
+        <div className='h-6 w-6'>
+          <img
+            src='https://www.gstatic.com/companion/icon_assets/contacts_2x.png'
+            alt='keep'
+          />
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default SidebarRight;
